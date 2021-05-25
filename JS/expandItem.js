bd5fc41beaa393245b417a0c29a9d49ad3cea98e
amaterasuzxc <igarjar@gmail.com>
2021-05-25 19:05:49 +0300

    Now only one item may be pop-up, added glow to special

--- JS/expandItem.js
+++ JS/expandItem.js
@@ -5,10 +5,11 @@ for (i = 0; i < call.length; i++) {
     call[i].addEventListener("click", function () {
         var item = this.parentElement;
         item.classList.toggle("activeItem");
-        // if (item.style.width === "47%") {
-        //     item.style.width = "90%";
-        // } else {
-        //     item.style.width = "47%";
-        // }
+        var j;
+        for (j = 0; j < call.length; j++) {
+            if (call[j] !== this) {
+                call.item(j).parentElement.classList.toggle("activeItem", false);
+            }
+        }
     });
 }
