67e5121c03656bcc859eaf0349d8582b479fbc7b
amaterasuzxc <igarjar@gmail.com>
2021-06-20 14:02:35 +0300

    Added all positions, descriptions

--- JS/expandItem.js
+++ JS/expandItem.js
@@ -6,7 +6,10 @@ var i;
 for (i = 0; i < call.length; i++) {
     call[i].addEventListener("click", function () {
         var item = this.parentElement;
+        var name = this.parentElement.getElementsByClassName("itemName").item(0);
+        console.log(name.className);
         item.classList.toggle("activeItem");
+        name.classList.toggle("activeItemName");
         fakeBack.classList.toggle("toggleFakeBack");
     });
 }
@@ -15,5 +18,6 @@ fakeBack.addEventListener("click", function () {
     fakeBack.classList.toggle("toggleFakeBack", false);
    for (i = 0; i < call.length; i++) {
        call.item(i).parentElement.classList.toggle("activeItem", false);
+       call.item(i).parentElement.getElementsByClassName("itemName").item(0).classList.toggle("activeItemName", false)
    } 
 });
