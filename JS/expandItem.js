863b81c53eabede937c560b70c68480c17c4fa08
amaterasuzxc <igarjar@gmail.com>
2021-06-20 14:52:32 +0300

    Fixes in scripts

--- JS/expandItem.js
+++ JS/expandItem.js
@@ -7,9 +7,10 @@ for (i = 0; i < call.length; i++) {
     call[i].addEventListener("click", function () {
         var item = this.parentElement;
         var name = this.parentElement.getElementsByClassName("itemName").item(0);
-        console.log(name.className);
+        var desc = this.parentElement.getElementsByClassName("itemDescription").item(0);
         item.classList.toggle("activeItem");
         name.classList.toggle("activeItemName");
+        desc.classList.toggle("activeItemDescription");
         fakeBack.classList.toggle("toggleFakeBack");
     });
 }
@@ -18,6 +19,7 @@ fakeBack.addEventListener("click", function () {
     fakeBack.classList.toggle("toggleFakeBack", false);
    for (i = 0; i < call.length; i++) {
        call.item(i).parentElement.classList.toggle("activeItem", false);
-       call.item(i).parentElement.getElementsByClassName("itemName").item(0).classList.toggle("activeItemName", false)
+       call.item(i).parentElement.getElementsByClassName("itemName").item(0).classList.toggle("activeItemName", false);
+       call.item(i).parentElement.getElementsByClassName("itemDescription").item(0).classList.toggle("activeItemDescription", false);
    } 
 });
